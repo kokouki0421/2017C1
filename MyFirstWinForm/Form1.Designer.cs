4612ee9db2dc5940e8f3a5d2a2f261a45df8e8fd
unknown <>
2017-02-17 16:30:58 +0800

    homework

---
+++ MyFirstWinForm/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MyFirstWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(245, 232);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(72, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "押して";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(633F, 1349F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(550, 433);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Inch, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Margin = new System.Windows.Forms.Padding(316, 270, 316, 270);
+            this.Name = "Form1";
+            this.Text = "おもしろい";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SimpleCalculator01/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /* double x = double.Parse(textBox1.Text);
+             double y = double.Parse(textBox2.Text);
+             label1.Text = (x + y).ToString();*/
+            input(1);
+        }
+
+     
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*double x = double.Parse(textBox1.Text);
+            double y = double.Parse(textBox2.Text);
+            label1.Text = (x - y).ToString();*/
+            input(1);
+        }
+        private void input(int i)
+        {
+            double x = double.Parse(textBox3.Text);
+            double y = double.Parse(textBox4.Text);
+            if (i==1)
+            {
+                label2.Text = (x + y).ToString(); 
+            }
+            else if(i==2)
+            {
+                label2.Text = (x - y).ToString();
+            }
+            else if(i==3)
+            {
+                label2.Text = (x * y).ToString();
+            }
+            else
+            {
+                label2.Text = (x / y).ToString();
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            input(2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            input(3);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            input(5);
+        }
+
+        private void button_Click1(object sender, EventArgs e)
+        {
+            input(1);
+        }
+    }
+}
